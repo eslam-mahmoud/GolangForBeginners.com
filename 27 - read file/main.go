aba5ff03d94f3c76e605e34a6c106b04c2406905
Eslam Mahmoud <contact@eslam.me>
2020-05-22 00:24:01 -0500

    update read file

--- 27 - read file/main.go
+++ 27 - read file/main.go
@@ -7,36 +7,36 @@ import (
 )
 
 func main() {
-	// {
-	// 	// Read full file in the memory
-	// 	// do not do this in production app
-	// 	bytes, err := ioutil.ReadFile("./test.json")
-	// 	if err != nil {
-	// 		panic(err)
-	// 	}
-	// 	fmt.Print(string(bytes))
-	// }
-	// {
-	// 	// read file in chunks
-	// 	f, err := os.Open("./test.json")
-	// 	if err != nil {
-	// 		panic(err)
-	// 	}
-	// 	defer f.Close()
-	// 	reader := bufio.NewReader(f)
-	// 	b := make([]byte, 5)
-	// 	for {
-	// 		numberOfBytesRead, err := reader.Read(b)
-	// 		if err != nil {
-	// 			if !errors.Is(err, io.EOF) {
-	// 				fmt.Println("Error reading file:", err)
-	// 			}
-	// 			break
-	// 		}
-	// 		fmt.Println(string(b[0:numberOfBytesRead]))
-	// 		// fmt.Println(string(b))
-	// 	}
-	// }
+	{
+		// Read full file in the memory
+		// do not do this in production app
+		bytes, err := ioutil.ReadFile("./test.json")
+		if err != nil {
+			panic(err)
+		}
+		fmt.Print(string(bytes))
+	}
+	{
+		// read file in chunks
+		f, err := os.Open("./test.json")
+		if err != nil {
+			panic(err)
+		}
+		defer f.Close()
+		reader := bufio.NewReader(f)
+		b := make([]byte, 5)
+		for {
+			numberOfBytesRead, err := reader.Read(b)
+			if err != nil {
+				if !errors.Is(err, io.EOF) {
+					fmt.Println("Error reading file:", err)
+				}
+				break
+			}
+			fmt.Println(string(b[0:numberOfBytesRead]))
+			// fmt.Println(string(b))
+		}
+	}
 	{
 		// read file line by line
 		f, err := os.Open("./test.json")
